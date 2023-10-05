2ab65c246b07dc8f4ff9251747598150ad211a98
J-Wyz <97983684+J-Wyz@users.noreply.github.com>
2023-10-05 07:07:06 -0400

    Update voteCounting.js

--- voteCounting.js
+++ voteCounting.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const bodyParser = require('body-parser');
 
 app.use(express.static('templates'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/store_vote', (req, res) => {
+app.post('/', function requestHandler(req, res) {
     const data = req.body;
     fs.appendFile('votes.txt', JSON.stringify(data) + '\n', (err) => {
         if (err) throw err;
         console.log('Data stored successfully');
-    });
-    res.send('Data stored successfully');
+        res.send('Data stored successfully');
+  });
 });
 
 app.listen(3000, () => {
